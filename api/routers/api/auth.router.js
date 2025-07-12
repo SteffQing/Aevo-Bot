360f9c2fffc77ead9e586ed852a59f1aa3229ed5
Steve Tomi <steveola23@gmail.com>
2025-07-12 23:13:00 +0100

    remove api

--- api/routers/api/auth.router.js
+++
@@ -1,8 +0,0 @@
-
-const express = require('express');
-const router = express.Router();
-const controller = require('../../controllers/api/auth.controller');
-
-router.post('/auth', controller.auth);
-
-module.exports = router;
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from aevo.sign_up import sign_up
 
 async def main():
-    await sign_up('0xcfd92db493a727a5142b3a28ba392ba0b942955de38e3180e3945b691bd892d5')
+    await sign_up('')
 
 if __name__ == "__main__":
     import asyncio
